f29d0da9249497fc32ebd23c0a57035c90994627
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-01-07 15:02:43 +0000

    Added Action Class Skeletons

---
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/HoldPosition.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Utilities;
+using MCTS.DST.WorldModels;
+using MCTS.DST;
+
+
+namespace MCTS.DST.Actions
+{
+
+    public class HoldPosition : ActionDST
+    {
+        public HoldPosition(string name) : base(name)
+        {
+        }
+
+        public override void ApplyActionEffects(WorldModelDST worldState)
+        {
+            base.ApplyActionEffects(worldState);
+        }
+
+        public override List<Pair<string, string>> Decompose(PreWorldState preWorldState)
+        {
+            return base.Decompose(preWorldState);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override Pair<string, int> NextActionInfo()
+        {
+            return base.NextActionInfo();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
