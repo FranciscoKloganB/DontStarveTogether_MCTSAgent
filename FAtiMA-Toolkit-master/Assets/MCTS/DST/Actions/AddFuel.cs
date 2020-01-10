a1cd7c43b78831e68b6887795ef0cbf3d43d62d6
RafaelMenesesRibeiro <rafaelmmribeiro@gmail.com>
2020-01-10 18:57:22 +0000

    Implemented AddFuel Action

--- FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/AddFuel.cs
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/AddFuel.cs
@@ -10,38 +10,41 @@ namespace MCTS.DST.Actions
 
     public class AddFuel : ActionDST
     {
-        public AddFuel(string name) : base(name)
+        private string Fuel;
+        private string Target;
+        private float Duration;
+        private static readonly string ActionName = "AddFuel_";
+
+        public AddFuel(string target) : base(ActionName + target)
         {
+            this.Target = target;
+            this.Duration = 0.33f;
         }
 
         public override void ApplyActionEffects(WorldModelDST worldState)
         {
-            base.ApplyActionEffects(worldState);
+            this.Fuel = worldState.Fuel[0].Item1;
+            worldState.Cycle += this.Duration;
+            worldState.RemoveFromPossessedItems(this.Fuel, 1);
+            worldState.RemoveFromFuel(this.Fuel);
+            worldState.Walter.Position = worldState.GetNextPosition(this.Target, "fire");
+            // TODO - Removed Construct actions that are now impossible due to loss of material.
         }
 
         public override List<Pair<string, string>> Decompose(PreWorldState preWorldState)
         {
-            return base.Decompose(preWorldState);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
+            string fuel = preWorldState.GetInventoryGUID(this.Fuel).ToString();
+            string fire = preWorldState.GetInventoryGUID(this.Target).ToString();
+            
+            return new List<Pair<string, string>>(1)
+            {
+                new Pair<string, string>("Action(ADDFUEL, " + fuel + ", -, -, -)", fire)
+            };
         }
 
         public override Pair<string, int> NextActionInfo()
         {
             return base.NextActionInfo();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
         }
     }
 }
