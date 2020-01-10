b7c20ef8c95841c36a7011800d086b3238597caf
RafaelMenesesRibeiro <rafaelmmribeiro@gmail.com>
2020-01-10 15:45:00 +0000

    Implemented Unequip Action

--- FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Unequip.cs
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Unequip.cs
@@ -10,38 +10,36 @@ namespace MCTS.DST.Actions
 
     public class Unequip : ActionDST
     {
-        public Unequip(string name) : base(name)
+        private string Target;
+        private float Duration;
+        private static readonly string ActionName = "Unequip_";
+
+        public Unequip(string target) : base(ActionName + target)
         {
+            this.Target = target;
+            this.Duration = 0.0f;
         }
 
         public override void ApplyActionEffects(WorldModelDST worldState)
         {
-            base.ApplyActionEffects(worldState);
+            worldState.Cycle += this.Duration;
+            worldState.RemoveFromEquipped(this.Target);
+            worldState.AddToPossessedItems(this.Target, 1);
+            ActionDST equipAction = new Equip(this.Target);
+            worldState.AddAction(equipAction);
         }
 
         public override List<Pair<string, string>> Decompose(PreWorldState preWorldState)
         {
-            return base.Decompose(preWorldState);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
+            return new List<Pair<string, string>>(1)
+            {
+                new Pair<string, string>("Action(Unequip, " + preWorldState.GetInventoryGUID(this.Target).ToString() + ", -, -, -)", "-")
+            };
         }
 
         public override Pair<string, int> NextActionInfo()
         {
             return base.NextActionInfo();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
         }
     }
 }
