aada46f98cf246e246e61a98ad99873e20efebab
RafaelMenesesRibeiro <rafaelmmribeiro@gmail.com>
2020-01-10 15:35:52 +0000

    Implemented Equip Action

--- FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Equip.cs
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Equip.cs
@@ -10,38 +10,40 @@ namespace MCTS.DST.Actions
 
     public class Equip : ActionDST
     {
-        public Equip(string name) : base(name)
+        private string Target;
+        private float Duration;
+        private static readonly string ActionName = "Equip_";
+
+        public Equip(string target) : base(ActionName + target)
         {
+            this.Target = target;
+            this.Duration = 0.0f;
         }
 
         public override void ApplyActionEffects(WorldModelDST worldState)
         {
-            base.ApplyActionEffects(worldState);
+            worldState.Cycle += this.Duration;
+            worldState.RemoveFromPossessedItems(this.Target, 1);
+            worldState.AddToEquipped(this.Target);
+            ActionDST unequipAction = new Unequip(this.Target);
+            worldState.AddAction(unequipAction);
+            if (!worldState.Possesses(this.Target))
+            {
+                worldState.RemoveAction(ActionName + this.Target);
+            }
         }
 
         public override List<Pair<string, string>> Decompose(PreWorldState preWorldState)
         {
-            return base.Decompose(preWorldState);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
+            return new List<Pair<string, string>>(1)
+            {
+                new Pair<string, string>("Action(Equip, " + preWorldState.GetInventoryGUID(this.Target).ToString() + ", -, -, -)", "-")
+            };
         }
 
         public override Pair<string, int> NextActionInfo()
         {
             return base.NextActionInfo();
-        }
-
-        public override string ToString()
-        {
-            return base.ToString();
         }
     }
 }
