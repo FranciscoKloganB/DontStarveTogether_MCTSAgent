5eb6842c3ee99d3c38e1cd5d2ab0e10991cacd61
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-01-10 19:55:50 +0000

    Code cleanup in Construct.cs

--- FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Construct.cs
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Construct.cs
@@ -27,7 +27,7 @@ namespace MCTS.DST.Actions
             {
                 Buildable targetBuildable = ItemBase[this.Target];
                 targetBuildable.Build(worldState);
-                targetBuildable.TryMakeUnequipable(worldState);
+                targetBuildable.PostProcessBuildable(worldState);
                 targetBuildable.TryRemoveAction(worldState, actionName);
                 worldState.Cycle += duration;
             }
@@ -38,73 +38,50 @@ namespace MCTS.DST.Actions
 
             if (this.Target == "torch")
             {
-                worldState.RemoveFromPossessedItems("twigs", 2);
-                worldState.RemoveFromPossessedItems("cutgrass", 2);
-                worldState.AddToPossessedItems("torch", 1);
                 if (!worldState.Possesses("twigs", 2))
                     worldState.RemoveAction("Build_pickaxe");
                 if (!worldState.Possesses("twigs", 1))
                     worldState.RemoveAction("Build_axe");
                 if (!worldState.Possesses("twigs", 2) || !worldState.Possesses("cutgrass", 2))
                     worldState.RemoveAction("Build_torch");
-                if (worldState.EquippedItems.Count != 0)
-                    return;
-                ActionDST action = (ActionDST)new Unequip("torch");
-                worldState.AddAction(action);
             }
             else if (this.Target == "pickaxe")
             {
-                worldState.RemoveFromPossessedItems("twigs", 2);
-                worldState.RemoveFromPossessedItems("flint", 2);
-                worldState.AddToPossessedItems("pickaxe", 1);
                 if (!worldState.Possesses("twigs", 2) || !worldState.Possesses("flint", 2))
                     worldState.RemoveAction("Build_pickaxe");
                 if (!worldState.Possesses("twigs", 1) || !worldState.Possesses("flint", 1))
                     worldState.RemoveAction("Build_axe");
-                if (worldState.Possesses("twigs", 2))
-                    return;
-                worldState.RemoveAction("Build_torch");
+                if (!worldState.Possesses("twigs", 2))
+                    worldState.RemoveAction("Build_torch");
+
             }
             else if (this.Target == "axe")
             {
-                worldState.RemoveFromPossessedItems("twigs", 1);
-                worldState.RemoveFromPossessedItems("flint", 1);
-                worldState.AddToPossessedItems("axe", 1);
                 if (!worldState.Possesses("twigs", 2) || !worldState.Possesses("flint", 2))
                     worldState.RemoveAction("Build_pickaxe");
                 if (!worldState.Possesses("twigs", 1) || !worldState.Possesses("flint", 1))
                     worldState.RemoveAction("Build_axe");
-                if (worldState.Possesses("twigs", 2))
-                    return;
-                worldState.RemoveAction("Build_torch");
+                if (!worldState.Possesses("twigs", 2))
+                    worldState.RemoveAction("Build_torch");
+
             }
             else if (this.Target == "campfire")
             {
-                worldState.RemoveFromPossessedItems("log", 2);
-                worldState.RemoveFromPossessedItems("cutgrass", 3);
-                worldState.AddToWorld("campfire", 1, worldState.Walter.Position.Item1, worldState.Walter.Position.Item2);
-                worldState.AddToFire("campfire", worldState.Walter.Position.Item1, worldState.Walter.Position.Item2);
+
                 if (!worldState.Possesses("log", 2))
                     worldState.RemoveAction("Build_firepit");
                 if (!worldState.Possesses("log", 2) || !worldState.Possesses("cutgrass", 3))
                     worldState.RemoveAction("Build_campfire");
-                if (worldState.Possesses("cutgrass", 2))
-                    return;
-                worldState.RemoveAction("Build_torch");
+                if (!worldState.Possesses("cutgrass", 2))
+                    worldState.RemoveAction("Build_torch");
             }
             else
             {
-                if (!(this.Target == "firepit"))
-                    return;
-                worldState.RemoveFromPossessedItems("log", 2);
-                worldState.RemoveFromPossessedItems("rocks", 12);
-                worldState.AddToWorld("firepit", 1, worldState.Walter.Position.Item1, worldState.Walter.Position.Item2);
-                worldState.AddToFire("firepit", worldState.Walter.Position.Item1, worldState.Walter.Position.Item2);
                 if (!worldState.Possesses("log", 2) || !worldState.Possesses("rocks", 12))
                     worldState.RemoveAction("Build_firepit");
-                if (worldState.Possesses("log", 2))
-                    return;
-                worldState.RemoveAction("Build_campfire");
+                if (!worldState.Possesses("log", 2))
+                    worldState.RemoveAction("Build_campfire");
+
             }
         }
 
