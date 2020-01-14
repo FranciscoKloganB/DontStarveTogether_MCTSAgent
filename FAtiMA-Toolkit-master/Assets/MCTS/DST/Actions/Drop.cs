b750d775494ebf58a2014e217c20518cbf639adb
RafaelMenesesRibeiro <rafaelmmribeiro@gmail.com>
2020-01-14 22:12:19 +0000

    Implemented Drop Action

---
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/Drop.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Utilities;
+using MCTS.DST.WorldModels;
+using MCTS.DST;
+using MCTS.DST.Resources.Materials;
+
+
+namespace MCTS.DST.Actions
+{
+
+    public class Drop : ActionDST
+    {
+        public string Target;
+        public float Duration;
+        private static readonly string actionName = "Drop_";
+
+        public Drop(string target) : base(actionName + target)
+        {
+            this.Target = target;
+            this.Duration = 0.33f;
+        }
+
+        public override void ApplyActionEffects(WorldModelDST worldState)
+        {
+            worldState.Cycle += this.Duration;
+            worldState.RemoveFromEquipped(this.Target);
+        }
+
+        public override List<Pair<string, string>> Decompose(PreWorldState preWorldState)
+        {
+            var x = preWorldState.Walter.GetPosX();
+            var z = preWorldState.Walter.GetPosZ();
+            return new List<Pair<string, string>>(1)
+            {
+                new Pair<string, string>("Action(DROP, " + preWorldState.GetEquippedGUID(this.Target).ToString() + ", " + x + ", " + z + ", -)", "-")
+            };
+        }
+
+        public override Pair<string, int> NextActionInfo()
+        {
+            return base.NextActionInfo();
+        }
+    }
+}
