1718d32f46878a5e7cfec0776888d48a98ab7d86
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2020-01-14 23:01:15 +0000

    Create NPCData

---
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Data/NPCData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MCTS.DST.WorldModels
+{
+
+    public class NPCData
+    {
+        public string NPCName { get; private set; }
+        public int PosX { get; private set; }
+        public int PosZ { get; private set; }
+
+        public NPCData(string name, int posX, int posZ)
+        {
+            this.NPCName = name;
+            this.PosX = posX;
+            this.PosZ = posZ;
+        }
+
+        public NPCData Clone()
+        {
+            return new NPCData(string.Copy(this.NPCName), this.PosX, this.PosZ);
+        }
+    } 
+}
