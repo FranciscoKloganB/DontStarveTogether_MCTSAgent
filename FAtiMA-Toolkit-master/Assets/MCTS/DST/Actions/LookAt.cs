34b50b22cb6c727bf0c5781a38f56aa218f1050e
RafaelMenesesRibeiro <rafaelmmribeiro@gmail.com>
2020-01-14 22:33:27 +0000

    Implemented LookAt Action

---
+++ FAtiMA-Toolkit-master/Assets/MCTS/DST/Actions/LookAt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Utilities;
+using MCTS.DST.WorldModels;
+using MCTS.DST;
+using MCTS.DST.Resources.Materials;
+
+
+namespace MCTS.DST.Actions
+{
+
+    public class LookAt : ActionDST
+    {
+        public string Target;
+        public float Duration;
+        private static readonly string actionName = "LookAt_";
+
+        public LookAt(string target) : base(actionName + target)
+        {
+            this.Target = target;
+            this.Duration = 0.0f;
+        }
+
+        public override void ApplyActionEffects(WorldModelDST worldState)
+        {
+            worldState.Cycle += this.Duration;
+        }
+
+        public override List<Pair<string, string>> Decompose(PreWorldState preWorldState)
+        {
+            return new List<Pair<string, string>>(1)
+            {
+                new Pair<string, string>("Action(LOOKAT, -, -, -, -)", preWorldState.GetEntitiesGUID(this.Target).ToString())
+            };
+        }
+
+        public override Pair<string, int> NextActionInfo()
+        {
+            return base.NextActionInfo();
+        }
+    }
+}
