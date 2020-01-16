6883ef547637730a4bc710c6e1874af477d360f5
RafaelMenesesRibeiro <rafaelmmribeiro@gmail.com>
2020-01-16 18:50:28 +0000

    Added Try Catch to get stacktrace

--- FAtiMA-Toolkit-master/Assets/MCTS/MCTSAsset.cs
+++ FAtiMA-Toolkit-master/Assets/MCTS/MCTSAsset.cs
@@ -58,64 +58,80 @@ namespace MCTS
         //This is where the main body of the MCTS Search must be implemented
         private IEnumerable<DynamicPropertyResult> MCTSSearch(IQueryContext context, Name actionVar, Name targetVar)
         {
-            //How to clone the KB with our JSON serializer
-            var jsonSerializer = new JSONSerializer();
-            var memStream = new MemoryStream();
-            var json = jsonSerializer.SerializeToJson(this.m_kb);
-            var kbCloned = jsonSerializer.DeserializeFromJson<KB>(json);
+            Substitution actionSub = null;
+            Substitution targetSub = null;
 
-            //Escrever comentário
-            
-            if (this.NextActionInfo.Item1 != "")
-            {               
-                NextAction PriorityAction = new NextAction(NextActionInfo.Item1, kbCloned);
-
-                if (this.NextActionInfo.Item2 <= 1)
-                {
-                    this.NextActionInfo = new Pair<string, int>("", 0);
-                }
-                else
-                {
-                    this.NextActionInfo.Item2 -= 1;
-                }
-
-                Pair<string, string> pairOfPriorityAction = PriorityAction.ConstructNextAction();
-                this.ToDoActionsList.Add(pairOfPriorityAction);
-            }
-
-            if (this.ToDoActionsList.Count == 0)
+            try
             {
-                PreWorldState preWorldState = new PreWorldState(kbCloned);
-                WorldModelDST worldModel = new WorldModelDST(preWorldState);
+                //How to clone the KB with our JSON serializer
+                var jsonSerializer = new JSONSerializer();
+                var memStream = new MemoryStream();
+                var json = jsonSerializer.SerializeToJson(this.m_kb);
+                var kbCloned = jsonSerializer.DeserializeFromJson<KB>(json);
 
-                Console.WriteLine("Available Actions: ");
+                //Escrever comentário
 
-                foreach (var action in worldModel.AvailableActions)
+                if (this.NextActionInfo.Item1 != "")
                 {
-                    Console.WriteLine("     " + action.Name);
+                    NextAction PriorityAction = new NextAction(NextActionInfo.Item1, kbCloned);
+
+                    if (this.NextActionInfo.Item2 <= 1)
+                    {
+                        this.NextActionInfo = new Pair<string, int>("", 0);
+                    }
+                    else
+                    {
+                        this.NextActionInfo.Item2 -= 1;
+                    }
+
+                    Pair<string, string> pairOfPriorityAction = PriorityAction.ConstructNextAction();
+                    this.ToDoActionsList.Add(pairOfPriorityAction);
                 }
+
+                if (this.ToDoActionsList.Count == 0)
+                {
+                    PreWorldState preWorldState = new PreWorldState(kbCloned);
+                    WorldModelDST worldModel = new WorldModelDST(preWorldState);
+
+                    Console.WriteLine("Available Actions: ");
+
+                    foreach (var action in worldModel.AvailableActions)
+                    {
+                        Console.WriteLine("     " + action.Name);
+                    }
+                    Console.WriteLine("");
+
+                    this.Mcts = new MCTSAlgorithm(worldModel);
+                    this.Mcts.InitializeMCTSearch();
+
+                    ActionDST MacroAction = this.Mcts.Run();
+
+                    this.ToDoActionsList = MacroAction.Decompose(preWorldState);
+
+                    this.NextActionInfo = MacroAction.NextActionInfo();
+                    this.NextActionInfo.Item1 = preWorldState.CompleteNextActionInfo(this.NextActionInfo.Item1);
+                }
+
+                Pair<string, string> CurrentAction = this.ToDoActionsList[0];
+                this.ToDoActionsList.Remove(CurrentAction);
+
+                Console.WriteLine("Next Action:");
+                Console.WriteLine(CurrentAction.Item1 + " " + CurrentAction.Item2);
                 Console.WriteLine("");
 
-                this.Mcts = new MCTSAlgorithm(worldModel);
-                this.Mcts.InitializeMCTSearch();
-
-                ActionDST MacroAction = this.Mcts.Run();
-             
-                this.ToDoActionsList = MacroAction.Decompose(preWorldState);
-
-                this.NextActionInfo = MacroAction.NextActionInfo();
-                this.NextActionInfo.Item1 = preWorldState.CompleteNextActionInfo(this.NextActionInfo.Item1);
+                actionSub = new Substitution(actionVar, new ComplexValue(Name.BuildName(CurrentAction.Item1)));
+                targetSub = new Substitution(targetVar, new ComplexValue(Name.BuildName(CurrentAction.Item2)));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("MCTSAsset.cs: " + ex.Message);
+                Console.WriteLine(ex.StackTrace);
             }
 
-            Pair<string, string> CurrentAction = this.ToDoActionsList[0];
-            this.ToDoActionsList.Remove(CurrentAction);
-
-            Console.WriteLine("Next Action:");
-            Console.WriteLine(CurrentAction.Item1 + " " + CurrentAction.Item2);
-            Console.WriteLine("");
-
-            var actionSub = new Substitution(actionVar, new ComplexValue(Name.BuildName(CurrentAction.Item1)));
-            var targetSub = new Substitution(targetVar, new ComplexValue(Name.BuildName(CurrentAction.Item2)));
+            if (actionSub is null || targetSub is null)
+            {
+                Console.WriteLine("MCTSAsset.cs - actionSub || targetSub is null");
+            }
 
             foreach (var subSet in context.Constraints)
             {
@@ -124,6 +140,7 @@ namespace MCTS
 
                 yield return new DynamicPropertyResult(new ComplexValue(Name.BuildName(true), 1.0f), subSet);
             }
+
         }
 
     }
